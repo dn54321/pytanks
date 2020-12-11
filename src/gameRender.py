b486007561d3e611421c18c642803aa246e8dcb5
Daniel Minh Pham <daniel_m_pham@hotmail.com>
2020-12-11 23:53:54 +1100

    completed a few functions for gameRender.py

--- src/gameRender.py
+++ src/gameRender.py
@@ -3,20 +3,33 @@ from src import constant
 from lib import utils
 from random import randint, shuffle
 class GameRender:
+    _direction = {
+        0: (0,0),
+        constant.UP: (0,-1),
+        constant.DOWN: (0,1),
+        constant.LEFT: (-1,0),
+        constant.RIGHT: (1,0),
+        constant.LEFT|constant.UP: (-1,-1),
+        constant.RIGHT|constant.UP: (1,-1),
+        constant.LEFT|constant.DOWN: (-1,1),
+        constant.RIGHT|constant.DOWN: (1,1)
+    }
+
     def __init__(self, surface, width, height):
         self._surface = surface
         self._sprite_sheet = self.get_tileset()
+        self._bitmap = None
         self._width = width
         self._height = height
-        self._pos = (0,0)
+        self._cursor_pos = (0,0)
 
     # Changes the pygame surface to render objects on
     def change_surface(self, surface):
         self._surface = surface
 
-    # Goes to row and column index
+    # Goes to row and column of tile to be placed.
     def move_cursor(self, x, y):
-        self._pos = (x,y)
+        self._cursor_pos = (x,y)
 
     # Gets 16x16 textures and upscales to 32x32
     def get_tileset(self):
@@ -30,7 +43,7 @@ class GameRender:
                 if y: tileset[x][y] = pygame.transform.scale2x(image.subsurface(rect))
         return tileset
     
-    def render_tile(self, obj, x=self._pos[0]*16, y=self._pos[1]*16):
+    def render_tile(self, obj, x=self._cursor_pos[0]*16, y=self._cursor_pos[1]*16):
         i,j = obj
         self._surface.blit(self._sprite_sheet[i][j], (x,y))
         
@@ -40,18 +53,40 @@ class GameRender:
 
     def get_bullet_sprite(self):
 
-    def get_wall_sprite(self, ascii_grid):
+    # Finds the correct tile wall sprite and returns the 2d index of it.
+    def _get_wall_sprite():
+        key, y_offset = 0, 0
+        if self._is_bit(0, [-1,0]): key |= constant.LEFT
+        elif self._is_bit(0, [-1,1]) and self._is_bit(1,[0,1]): key |= constant.LEFT
+        if self._is_bit(0, [0,-1]): key |= constant.UP
+        if self._is_bit(0, [1,0]) key |= constant.RIGHT
+        elif self._is_bit(0, [1,1]) and self._is_bit(1,[0,1]): key |= constant.RIGHT
+        if self._is_bit(0, [0,1]): y_offset = 4
+        elif self._is_bit(0,[0,2]): key |= constant.DOWN
+        i,j = GameRender._direction[key]
+        return i+7, j+1+y_offset
+            
+    def get_shadow_sprite(self, bitmap):
 
-    def get_shadow_sprite(self, ascii_grid):
-
+    # Fills the surface with wall tiles.  
+    def render_wall(self, bitmap):
+        self._bitmap = bitmap
+        for i in range(self.width):
+            for  j in range(self.height):
+                if bitmap[i][j]:
+                    self.move_cursor(i,j)
+                    tile = self._get_wall_sprite()
+                    self._render_tile(tile)
+                    
+    # Fills the surface with grass tiles.
     def render_background(self):
-        gz = constant.GRID_SIZE
         for i in range(self.width):
             for j in range(self.height):
                 tile = (randint(0,5), 1)
                 self.move_cursor(i,j)
                 self.render_tile(tile)
 
+    # Places some flowers at valid tile posiitons specified in bitmap
     def render_flowers(self, bitmap):
         has_flower = utils.array_2d(self.width, self.height, False)
 
@@ -80,5 +115,11 @@ class GameRender:
                 self.move_cursor(i,j)
                 self.render_tile(flower_tile)
     
-    def has(ascii_type, offset):
-    
+    # checks if the bit of the bitmap at position self._cursor_pos with some offset [offset]
+    # is equal to [bit]. Note that this arguement returns false if this position is out of bounds.
+    def _bit_check(is_bit, offset=[0,0], bitmap=self._bitmap): 
+        x = self._cursor_pos[0] + offset[0] 
+        y = self._cursor_pos[1] + offset[1]
+        if 0 <= x < self.width and 0 <= y < self.height:
+            return bitmap[x][y] == bit
+        return False
